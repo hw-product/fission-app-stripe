11869d347d81b62d28af8e164233d470d65b1f33
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-02 20:25:21 -0800

    Do not run stripe initialization if in rake task

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,10 @@
-require 'stripe'
+unless(defined?(Rake))
+  require 'stripe'
 
-if(ENV['STRIPE_SECRET_KEY'] && ENV['STRIPE_PUBLISHABLE_KEY'])
-  Rails.application.config.stripe_publish_key = ENV['STRIPE_PUBLISHABLE_KEY']
-  Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-else
-  raise 'Failed to configure stripe! Ensure STRIPE_SECRET_KEY and STRIPE_PUBLISHABLE_KEY environment variables are set!'
+  if(ENV['STRIPE_SECRET_KEY'] && ENV['STRIPE_PUBLISHABLE_KEY'])
+    Rails.application.config.stripe_publish_key = ENV['STRIPE_PUBLISHABLE_KEY']
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+  else
+    raise 'Failed to configure stripe! Ensure STRIPE_SECRET_KEY and STRIPE_PUBLISHABLE_KEY environment variables are set!'
+  end
 end
