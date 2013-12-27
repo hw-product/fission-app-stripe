eee85160ab91dfc8317163749731bee3bd6d0e1d
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-27 11:39:30 -0800

    Remove configuration set for deprecated json file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,1 @@
 require 'stripe'
-
-Rails.application.config.fission_packages_json = ENV.fetch(
-  'FISSION_PACKAGES_JSON', File.join(File.dirname(File.dirname(__FILE__)), 'fission_packages.json')
-)
