d55d0da5dc1e8acef61c6bae3fd5d3ffa75a551e
Chris Roberts <chrisroberts.code@gmail.com>
2014-08-07 10:04:15 -0700

    Add route for loading product features into form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   namespace :admin do
     namespace :stripe do
       resources :subscriptions
-      resources :plans
+      resources :plans do
+        collection do
+          get :load_product_features
+        end
+      end
       resources :customers
       resources :coupons
       resources :discounts
