4ec8f17a8d76c1e8b6fdebc244f0f7770f59f3c7
Chris Roberts <chrisroberts.code@gmail.com>
2014-08-07 06:43:38 -0700

    Update routing to provide admin access and nest subscriptions into account resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  scope :orders do
-    get '/', :to => 'stripe#index', :as => :order
-    get 'new', :to => 'stripe#new', :as => :new_order
-    post 'new', :to => 'stripe#create', :as => :create_order
-    get 'edit', :to => 'stripe#edit', :as => :edit_order
-    post 'edit', :to => 'stripe#update', :as => :update_order
-    delete 'destroy', :to => 'stripe#destroy', :as => :destroy_order
-    get 'confirmation', :to => 'stripe#confirmation', :as => :order_confirmation
-    post 'hook', :to => 'stripe#hook', :as => :order_hook
+  namespace :admin do
+    namespace :stripe do
+      resources :subscriptions
+      resources :plans
+      resources :customers
+      resources :coupons
+      resources :discounts
+    end
+  end
+  resources :accounts do
+    resources :subscriptions
   end
 end
