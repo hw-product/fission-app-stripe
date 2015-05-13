7dd418a257312f7c85fe8bd63fe44a5f09dbf8f4
Chris Roberts <code@chrisroberts.org>
2015-05-13 10:51:42 -0700

    Start the overhaul at the bottom

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
-    namespace :stripe do
-      resources :subscriptions
-      resources :plans do
-        collection do
-          get :load_product_features
-        end
-      end
-      resources :customers
-      resources :coupons
-      resources :discounts
-    end
+    resources :plans
   end
   namespace :account do
     resources :subscriptions
   end
-  get '/pricing', :to => 'stripe#pricing', :as => :pricing
+  get '/plans', :to => 'stripe#pricing', :as => :pricing
 end
