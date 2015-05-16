0367683bb72f72a5e0a165a94795ec99a348a2da
Chris Roberts <code@chrisroberts.org>
2015-05-16 08:31:30 -0700

    Include plan id with order type routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :account do
     get 'billing/details', :to => 'billing#display', :as => :billing_details
-    post 'billing/order', :to => 'billing#order', :as => :billing_order
-    post 'billing/upgrade', :to => 'billing#upgrade', :as => :billing_upgrade
-    post 'billing/downgrade', :to => 'billing#downgrade', :as => :billing_downgrade
+    post 'billing/order/:plan_id', :to => 'billing#order', :as => :billing_order
+    post 'billing/upgrade/:plan_id', :to => 'billing#upgrade', :as => :billing_upgrade
+    post 'billing/downgrade/:plan_id', :to => 'billing#downgrade', :as => :billing_downgrade
   end
   get '/plans', :to => 'stripe#pricing', :as => :pricing
 end
