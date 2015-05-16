04d85f4c49103f1d60167d39c31435bbe9bea1ae
Chris Roberts <code@chrisroberts.org>
2015-05-15 17:13:32 -0700

    Add billing related routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :account do
-    get 'billing', :to => 'billing#display', :as => :billing
+    get 'billing/details', :to => 'billing#display', :as => :billing_details
+    post 'billing/order', :to => 'billing#order', :as => :billing_order
+    post 'billing/upgrade', :to => 'billing#upgrade', :as => :billing_upgrade
+    post 'billing/downgrade', :to => 'billing#downgrade', :as => :billing_downgrade
   end
   get '/plans', :to => 'stripe#pricing', :as => :pricing
 end
