1dcb64f4764072e13885015e62f7b36b273e3b65
Chris Roberts <code@chrisroberts.org>
2015-05-21 09:26:07 -0700

    Update files for release v0.2.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.2.4
+* Refactor so every account has custom plan
+* Add lots of plan related stuff
+
 ## v0.2.2
 * View refactor
 
--- lib/fission-app-stripe/version.rb
+++ lib/fission-app-stripe/version.rb
@@ -1,5 +1,5 @@
 module FissionApp
   module Stripe
-    VERSION = Gem::Version.new('0.2.3')
+    VERSION = Gem::Version.new('0.2.4')
   end
 end
