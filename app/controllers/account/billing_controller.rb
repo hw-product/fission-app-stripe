705aa91afd57d5711afbb7e8b6938bc23fadf99b
Chris Roberts <code@chrisroberts.org>
2015-11-13 09:13:03 -0800

    Allow modification to current subscription plan (and full removal)

--- app/controllers/account/billing_controller.rb
+++ app/controllers/account/billing_controller.rb
@@ -11,7 +11,7 @@ class Account::BillingController < ApplicationController
     respond_to do |format|
       format.js do
         flash[:error] = 'Unsupported request!'
-        javascript_redirect_to dashboard_url
+        javascript_redirect_to account_billing_details_path
       end
       format.html do
         c_payment = @account.customer_payment
@@ -38,7 +38,7 @@ class Account::BillingController < ApplicationController
       end
       format.html do
         flash[:error] = 'Unsupported request!'
-        redirect_to dashboard_url
+        redirect_to account_billing_details_path
       end
     end
   end
@@ -47,7 +47,7 @@ class Account::BillingController < ApplicationController
     respond_to do |format|
       format.js do
         flash[:error] = 'Unsupported request!'
-        javascript_redirect_to dashboard_url
+        javascript_redirect_to account_billing_details_path
       end
       format.html do
         if(@account.customer_payment)
@@ -94,7 +94,7 @@ class Account::BillingController < ApplicationController
             flash[:error] = 'Failed to locate requested plan!'
           end
         end
-        redirect_to dashboard_url
+        redirect_to account_billing_details_path
       end
     end
   end
@@ -103,7 +103,7 @@ class Account::BillingController < ApplicationController
     respond_to do |format|
       format.js do
         flash[:error] = 'Unsupported request!'
-        javascript_redirect_to dashboard_url
+        javascript_redirect_to account_billing_details_path
       end
       format.html do
         current_plans = Plan.where(:id => @account.customer_payment.plan_ids).all
@@ -114,7 +114,7 @@ class Account::BillingController < ApplicationController
         else
           flash[:error] = 'Failed to locate requested plan!'
         end
-        redirect_to dashboard_url
+        redirect_to account_billing_details_path
       end
     end
   end
@@ -123,7 +123,7 @@ class Account::BillingController < ApplicationController
     respond_to do |format|
       format.js do
         flash[:error] = 'Unsupported request!'
-        javascript_redirect_to dashboard_url
+        javascript_redirect_to account_billing_details_path
       end
       format.html do
         current_plans = Plan.where(:id => @account.customer_payment.plan_ids).all
@@ -134,12 +134,53 @@ class Account::BillingController < ApplicationController
         else
           flash[:error] = 'Failed to locate requested plan!'
         end
-        redirect_to dashboard_url
+        redirect_to account_billing_details_path
+      end
+    end
+  end
+
+  def modify_existing
+    respond_to do |format|
+      format.js do
+        desired_plan_ids = [params[:plan_ids]].flatten.compact.uniq
+        current_plans = Plan.where(:id => @account.customer_payment.plan_ids).all
+        current_plan_ids = current_plans.map(&:id)
+        if(desired_plan_ids.map(&:to_s).sort == current_plan_ids.map(&:to_s).sort)
+          flash[:warn] = 'No updates detected to apply!'
+        else
+          stripe_info = stripe_account_information
+          desired_plans = Plan.where(:id => desired_plan_ids).all
+          set_account_plans(desired_plans, stripe_info)
+          flash[:success] = "Subscriptions have been successfully modified!"
+        end
+        javascript_redirect_to account_billing_details_path
+      end
+      format.html do
+        flash[:error] = 'Unsupported request!'
+        redirect_to account_billing_details_path
       end
     end
   end
 
   protected
+
+  # Load available account stripe information
+  #
+  # @return [Hash]
+  def stripe_account_information
+    payment = @account.customer_payment
+    stripe_customer = Stripe::Customer.retrieve(@account.customer_payment.customer_id)
+    current_stripe_subscription = stripe_customer.subscriptions.all.first
+    if(current_stripe_subscription)
+      current_stripe_plan = current_stripe_subscription.plan
+    end
+    Smash.new(
+      :payment => payment,
+      :customer => stripe_customer,
+      :subscription => current_stripe_subscription,
+      :plan => current_stripe_plan
+    )
+  end
 
   def modify_plan(upgrade_plan, current_plans)
     matching_plans = current_plans.find_all do |c_plan|
@@ -185,6 +226,57 @@ class Account::BillingController < ApplicationController
       Rails.logger.error "Failed to remove deprecated account plan: #{e.class} - #{e}"
     end
     @plan = upgrade_plan
+  end
+
+  def set_account_plans(plans, stripe_info)
+    unless(plans.empty?)
+      stripe_plan = Stripe::Plan.create(
+        :id => SecureRandom.uuid,
+        :name => "Heavy Water Products plan for account: #{@account.name}",
+        :amount => plans.map{|pln| pln.generated_cost(:integer)}.inject(&:+),
+        :currency => 'usd',
+        :interval => 'month',
+        :metadata => {
+          :fission_account_id => @account.id,
+          :fission_plans => plans.map(&:id).map(&:to_s).join(',')
+        }
+      )
+      stripe_info[:payment].metadata[:breakdown] ||= Smash.new
+      stripe_info[:payment].metadata[:breakdown][:plans] = Smash.new.tap do |all_plans|
+        plans.each do |plan|
+          all_plans[plan.id.to_s] = {
+            :name => plan.name,
+            :cost => plan.generated_cost(&:integer)
+          }
+        end
+      end
+      stripe_info[:payment].save
+      if(stripe_info[:subscription])
+        stripe_info[:subscription].plan = stripe_plan.id
+        stripe_info[:subscription].save
+      else
+        stripe_info[:subscription] = stripe_info[:customer].subscription.create(:plan => stripe_plan.id)
+      end
+      if(stripe_info[:plan])
+        begin
+          stripe_info[:plan].delete
+        rescue Stripe::InvalidRequestError => e
+          Rails.logger.error "Failed to remove deprecated account plan: #{e.class} - #{e}"
+        end
+      end
+      stripe_info[:plan] = stripe_plan
+    else
+      stripe_info[:payment].metadata[:breakdown] ||= Smash.new
+      stripe_info[:payment].metadata[:breakdown][:plans] = Smash.new
+      stripe_info[:payment].save
+      [:subscription, :plan].each do |k|
+        if(stripe_info[k])
+          stripe_info[k].delete
+          stripe_info.delete(k)
+        end
+      end
+    end
+    stripe_info
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :account do
     get 'billing/details', :to => 'billing#details', :as => :billing_details
+    post 'billing/modify', :to => 'billing#modify_existing', :as => :billing_modify
     post 'billing/card/edit', :to => 'billing#card_edit', :as => :billing_edit
     post 'billing/order/:plan_id', :to => 'billing#order', :as => :billing_order
     post 'billing/upgrade/:plan_id', :to => 'billing#upgrade', :as => :billing_upgrade
